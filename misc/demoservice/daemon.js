833de9373a7f85b8f3f56a8bd9eaecf292e31216
Christian Albrecht <christian.albrecht@mayflower.de>
2013-03-01 14:15:53 +0100

    Hust, Number err. !== string in daemon.js

--- misc/demoservice/daemon.js
+++ misc/demoservice/daemon.js
@@ -5,8 +5,8 @@ var resourceful = require('resourceful'),
     Track = resourceful.define('track');
 
 Track.property('description', String, { default: "no description" });
-Track.property('start',       Number, { format: 'date-time' });
-Track.property('end',         Number, { format:  'date-time' });
+Track.property('start',       String, { format: 'date-time' });
+Track.property('end',         String, { format:  'date-time' });
 Track.property('duration',    Number, { default: 0, minimum: 0, maximum: 3600 });
 
 // Setup a standalone restful server
